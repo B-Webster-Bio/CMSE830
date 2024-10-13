c3cc12ca7fc71df816f7aecd696f9cb5a56a66b7
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-10-13 09:18:11 -0400

    Update StreamApp.py

--- Project/StreamApp.py
+++ Project/StreamApp.py
@@ -2,13 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import hiplot as hip
-import json
 import os
 
 st.write(f"Current working directory: {os.getcwd()}")
 df = pd.read_csv('Project/Data/PlotFieldData.csv')
 
-df_forplot = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'KERNELDRYWT_PERPLANT']]
+df_forplot = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'gsw', 'Ci']]
 st.scatter_chart(data=df_forplot, x = 'E', y = 'A', color='NTREATMENT')
 st.scatter_chart(data=df_forplot, x = 'KERNELDRYWT_PERPLANT', y = 'A', color='NTREATMENT')
 
