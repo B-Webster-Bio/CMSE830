53fc6776cce0ca352362be19e8d501e3414d80b1
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-10-13 11:09:40 -0400

    Update StreamApp.py

--- Project/StreamApp.py
+++ Project/StreamApp.py
@@ -5,22 +5,24 @@ import hiplot as hip
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title('Gas Exchange in Corn Hybrids')
+st.title('Gas Exchange in Corn Inbreds')
 st.image('Project/Supp/LeafGasEx.png', caption = 'Gas exchange through leaf stomata')
 
 st.subheader('Gas exchange can be measured by a Licor')
-st.image('Project/Supp/Licor.PNG', caption = 'Licor 6800 measuring plant in the field')
-# Note that when hosted on community cloud the root directory defaults to the top level of github dir
+st.image('Project/Supp/Licor.PNG', caption = 'Licor 6800 measuring plant leaf in the field')
+
 md = ''' * A = CO2 assimilation rate (µmol CO2 m⁻² s⁻¹)  
  * E = Transpiration of H2O (mol H2O m⁻² s⁻¹)  
- * Gsw = stomatal conductance (mol H2O m⁻² s⁻¹)  
+ * gsw = stomatal conductance (mol H2O m⁻² s⁻¹)  
  * Ci = interceullar CO2 concentration ready for assimilation (ppm)
 '''
 st.markdown(md)
 
+# Note that when hosted on community cloud the root directory defaults to the top level of github dir
 df = pd.read_csv('Project/Data/PlotFieldData.csv')
 
-df_forplot = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'gsw', 'Ci']]
+df_b73 = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'gsw', 'Ci']]
+df_forplot = df.loc[:,['NTREATMENT', 'A', 'E', 'gsw', 'Ci']]
 f1 = sns.pairplot(df_forplot, hue = 'NTREATMENT')
 st.pyplot(fig = f1)
 
