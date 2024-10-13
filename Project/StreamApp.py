4cc17a8488dcf1b18575bc17561f7860df94799d
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-10-12 23:01:53 -0400

    Update StreamApp.py

--- Project/StreamApp.py
+++ Project/StreamApp.py
@@ -7,9 +7,9 @@ import json
 df = pd.read_csv('Data/PlotFieldData.csv')
 
 df_forplot = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'KERNELDRYWT_PERPLANT']]
-#st.scatter_chart(data=df_forplot, x = 'E', y = 'A', color='NTREATMENT')
-#st.scatter_chart(data=df_forplot, x = 'KERNELDRYWT_PERPLANT', y = 'A', color='NTREATMENT')
+st.scatter_chart(data=df_forplot, x = 'E', y = 'A', color='NTREATMENT')
+st.scatter_chart(data=df_forplot, x = 'KERNELDRYWT_PERPLANT', y = 'A', color='NTREATMENT')
 
-st.write('B73 ONLY')
-xp = hip.Experiment.from_dataframe(df_forplot)
-st.components.v1.html(xp.to_html(), height=1500)
+#st.write('B73 ONLY')
+#xp = hip.Experiment.from_dataframe(df_forplot)
+#st.components.v1.html(xp.to_html(), height=1500)
