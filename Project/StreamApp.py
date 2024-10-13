8cbef87468b32d5225e271414bcaeb36d55519eb
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-10-12 23:05:53 -0400

    Update StreamApp.py

--- Project/StreamApp.py
+++ Project/StreamApp.py
@@ -6,7 +6,7 @@ import json
 import os
 
 st.write(f"Current working directory: {os.getcwd()}")
-df = pd.read_csv('Data/PlotFieldData.csv')
+df = pd.read_csv('Project/Data/PlotFieldData.csv')
 
 df_forplot = df.loc[df['GENOTYPE'] == 'B73',['NTREATMENT', 'A', 'E', 'KERNELDRYWT_PERPLANT']]
 st.scatter_chart(data=df_forplot, x = 'E', y = 'A', color='NTREATMENT')
