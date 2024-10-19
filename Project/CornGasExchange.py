59965e5e5e5bf9aef069aaf14d6436703f9d2624
B-Webster-Bio <85455564+B-Webster-Bio@users.noreply.github.com>
2024-10-19 11:36:43 -0400

    Update CornGasExchange.py

--- Project/CornGasExchange.py
+++ Project/CornGasExchange.py
@@ -8,8 +8,9 @@ from sklearn.preprocessing import LabelEncoder
 
 st.title('Gas Exchange in Corn Inbreds')
 md = '''"Breathing" or gas exchange of Carbon Dioxide (CO2) and water (H2O) through leaf stomata is an important process in plant biology that is 
-associated with how much carbon and water a plant is processing. Nitrogen fertilizer is an important agricultural input that influences 
-the total amount of corn grain production. Gas exchange and grain yield were collected from a panel of 105 maize inbreds in high and low N treatments. 
+associated with how much carbon and water a plant is processing. In theory these traits are related to grain production but harnessing this information for plant breeding is tricky. 
+Nitrogen fertilizer is an important input that is well known to influence the total amount of grain production. 
+Gas exchange and grain yield were collected from a panel of 105 corn inbreds in high and low N treatments. 
 Understanding how gas exchange traits and yield change between N treatments over a diverse population of maize genotypes will enable better breeding for effecient corn lines.
 '''
 st.markdown(md)
@@ -48,13 +49,39 @@ st.subheader('Pairplot')
 st.pyplot(sns.pairplot(df_forplot, hue = 'NTREATMENT'))
 
 st.subheader('Correlations')
-df_cor = df_forplot.loc[:,['NTREATMENT', 'A', 'E', 'gsw', 'Ci', 'KERNELDRYWT_PERPLANT']]
-# Encode NTreatment as 1 for H or 0 for L to use in corr analysis
-label_map = {'H': 1, 'L': 0}
-df_cor['NTREATMENT'] = np.array([label_map[label] for label in df_cor['NTREATMENT']])
-f3, ax = plt.subplots()
-sns.heatmap(df_cor.corr(), ax=ax, annot=True, vmin = -1, vmax = 1)
-st.pyplot(f3)
+
+# Create three tabs
+tab1, tab2, tab3 = st.tabs(["All Data", "High N Treatment", "Low N Treatment"])
+
+# Function to create and display heatmap for tabs
+def plot_heatmap(data, title):
+    fig, ax = plt.subplots(figsize=(10, 8))
+    sns.heatmap(data.corr(), ax=ax, annot=True, vmin=-1, vmax=1, cmap='coolwarm')
+    plt.title(title)
+    st.pyplot(fig)
+
+# Content for Tab 1: All Data
+with tab1:
+    st.header("Correlation Heatmap - All Data")
+    df_cor = df_forplot.loc[:, ['NTREATMENT', 'A', 'E', 'gsw', 'Ci', 'KERNELDRYWT_PERPLANT']]
+    label_map = {'H': 1, 'L': 0}
+    df_cor['NTREATMENT'] = df_cor['NTREATMENT'].map(label_map)
+    plot_heatmap(df_cor, "Correlation Heatmap - All Data")
+
+# Content for Tab 2: High N Treatment
+with tab2:
+    st.header("Correlation Heatmap - High N Treatment")
+    df_cor_h = df_forplot.loc[df_forplot['NTREATMENT'] == 'H', ['NTREATMENT', 'A', 'E', 'gsw', 'Ci', 'KERNELDRYWT_PERPLANT']]
+    df_cor_h['NTREATMENT'] = df_cor_h['NTREATMENT'].map(label_map)
+    plot_heatmap(df_cor_h, "Correlation Heatmap - High N Treatment")
+
+# Content for Tab 3: Low N Treatment
+with tab3:
+    st.header("Correlation Heatmap - Low N Treatment")
+    df_cor_l = df_forplot.loc[df_forplot['NTREATMENT'] == 'L', ['NTREATMENT', 'A', 'E', 'gsw', 'Ci', 'KERNELDRYWT_PERPLANT']]
+    df_cor_l['NTREATMENT'] = df_cor_l['NTREATMENT'].map(label_map)
+    plot_heatmap(df_cor_l, "Correlation Heatmap - Low N Treatment")
+
 st.markdown("____")
 st.subheader('Preliminary Results:')
 st.markdown('''There appears to be a mildy strong association between yield and Nitrogen Treatment, this is well known. 
